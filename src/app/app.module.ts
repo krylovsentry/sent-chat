2e8e7b99ca372854c88b2e6f389b3b132add3ad8
Krylovsentry <anton.krylov@netcracker.com>
2020-01-19 03:23:58 +0300

    'asd'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+  url: window.location.hostname, options: {}
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +17,11 @@ import {FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
